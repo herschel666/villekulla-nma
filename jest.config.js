ba0854f67f890566ecdb281e049626786ad41200
Emanuel Kluge <mail@emanuel-kluge.de>
2020-05-21 13:50:26 +0200

    chore: fix babel config collision between gatsby & jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  transform: {
+    '\\.tsx?$': [
+      'babel-jest',
+      { configFile: './do-not-call-it-babel.config.js' },
+    ],
+  },
   moduleNameMapper: {
     '.+\\.css$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
